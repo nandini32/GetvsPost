f5467ef5fb3bc57d7233587fe0c09cb27a613550
nandini32 <58420860+nandini32@users.noreply.github.com>
2022-01-27 09:59:32 +0530

    Add files via upload

---
+++ ServletInterfaceDemo.java
@@ -0,0 +1,44 @@
+package com.phase2.training;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servletinterface")
+public class ServletInterfaceDemo extends HttpServlet {
+	
+	   ServletConfig config=null;
+	    public void init(ServletConfig config){
+	        this.config=config;
+	        System.out.println("Initialization complete");
+	     }
+
+	     public void service(ServletRequest request,ServletResponse response)
+	     throws IOException,ServletException{
+	         response.setContentType("text/html");
+	         PrintWriter out=response.getWriter();
+	         out.print("<html>");
+	         out.print("<body>");
+	         out.print("In the service() method<br>");
+	         out.print("</body>");
+	         out.print("</html>");
+	     }
+	     public void destroy(){
+	         System.out.println("In destroy() method");
+	     }
+	     public ServletConfig getServletConfig(){
+	         return config;
+	     }
+	     public String getServletInfo(){
+	         return "This is a sample servlet info";
+	     }
+
+}
