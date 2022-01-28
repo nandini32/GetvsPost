cce10cd54ccc10527586a89d8c741ac14d6b54cc
nandini32 <58420860+nandini32@users.noreply.github.com>
2022-01-28 17:30:37 +0530

    Add files via upload

--- Dashboard.java
+++ Dashboard.java
@@ -1,4 +1,4 @@
-package com.phase2.training;
+
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,23 +10,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 @WebServlet("/dashboard")
 public class Dashboard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-	    HttpSession session = request.getSession();
-	    PrintWriter out = response.getWriter();
-	    if(session.getAttribute("username")!=null) {
-	    out.println("<h3> Welcome "+session.getAttribute("username")+"</h3>");
-	    out.println("<br><h3> You have Logged in now....</h3>");
-	    out.println("<p><h4><a href='logout'>LOGOUT</a></h4>");
-	    
-	}
-	    else {
-	    	response.sendRedirect("Session.html");
-	    }
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+		response.setContentType("text/html");
+
+		HttpSession session = request.getSession();
+
+		PrintWriter out = response.getWriter();
+
+
+		if (session.getAttribute("userName") != null) {
+
+			out.println("<h3>Welcome " + session.getAttribute("userName"));
+
+			out.println("<br> You are now logged in...</h3>");
+
+			out.println("<p><h4><a href='logout'>LOGOUT</a></h4>");
+		}
+
+		else {
+			response.sendRedirect("index.html");
+		}     
+	}
 }
